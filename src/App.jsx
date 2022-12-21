7c479bd5a254bccffb21dae3b462ab2870981612
Kim Kamilla <kamila258568@gmail.com>
2022-12-21 15:35:23 +0300

    первая фиксация

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,46 @@
-import React from "react";
-import { useSelector , useDispatch } from "react-redux";
-import { addTodo , removeTodo } from "./action/index";
-import { useState } from "react";
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import store from './redux';
+import { addTodo, changeTodo, deleteTodo, importantTodo } from './redux/reducers/todo';
 
 function App() {
+  const dispatch = useDispatch()
+  const todos = useSelector((store) => store.todos.todos)
 
-  const [counter , setCounter] = useState(0)
-  const [todo , setTodo] = useState("");
-  const todos = useSelector(state => state.todos)
-  const dispatch = useDispatch();
-
-  const todoSubmitHandler = () => {
-    if (todo !== "") {
-      dispatch(addTodo(counter , todo))
-      setCounter(counter + 1)
-      setTodo("")
-    }
-  }
-
-  return (
-  <>
-        <h1>Add your ToDo List here</h1>
-      <input
-        className="inputAdd" 
-        type="text" 
-        value={todo} 
-        onChange={e => setTodo(e.target.value)}/>
+  const [todo , setTodo] = useState("")
+  const [change, setChange] = useState("")
   
-    <button
-      className="buttonAdd"
-      onClick={todoSubmitHandler}>OK</button>
-      
-
-    <div className="todos">
-      <ul>
-        {
-          todos?.map((todo) => (
-            <li className="todo" key={todo.id}>
-              <p>{todo.task}</p>
-              <button onClick={() => dispatch(removeTodo(todo.id))}>OK</button>
-            </li>
-          ))
-        }
-      </ul>
+  return (
+    <div className="App">
+        <div>
+          <input onChange={(e) => setTodo(e.target.value)} value={todo} type="text"/>
+          <button type='button' onClick={() => {dispatch(addTodo(todo))
+          setTodo("")
+          }}>ADD</button>
+          <div>
+            <input type="search"/>
+            <button>FIND</button>
+            {/* <button type='button' onClick={() => dispatch(importantTodo(item.id))}>important</button> */}
+          </div>
+        </div>
+        <ul>
+          {
+            todos.map((item) => (
+              <li style={{margin: '20px 0', color: item.isImportant ? 'red' : ''} }  key={item.id}>{item.title} 
+              <button type='button' style={{margin:'0 40px'}} onClick={() => {dispatch(deleteTodo(item.id))}}>delete</button> 
+              <button type='button' onClick={() => dispatch(importantTodo(item.id))}>important</button>
+              <button type="button" onClick={() => {dispatch(changeTodo(item.id, setTodo(item.title)))}}>change</button>
+                </li>
+            ))
+          }
+        </ul>
+        <div>
+          <input onChange={(e) => setChange(e.target.value)} value={change} type="text"/>
+          <button type='button' onClick={() => {dispatch(changeTodo(change))
+          setChange("")
+          }}>CHANGE</button>
+        </div>
     </div>
-    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from "./store"
-import { createStore } from 'redux';
-
-// import reducer from './Reducers/reducer';
+import store from './redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store ={store}>
-    <App />
-    </Provider>
+  <Provider store={store}> 
+    <App/>
+  </Provider>
+
 );
